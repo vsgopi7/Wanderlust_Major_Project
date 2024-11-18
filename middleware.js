28ebebf45be7de1fc805d9ca9186dfec6c85e6a5
M Venkata Sai Gopi <150466906+vsgopi7@users.noreply.github.com>
2024-11-19 03:24:20 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 const Listing = require("./models/listing");
 const Review = require("./models/review");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema,reviewSchema}=require("./Schema.js");
+const {listingSchema,reviewSchema}=require("./schema.js");
 
 module.exports.isLoggedIn=(req,res,next)=>{
     if(!req.isAuthenticated()){
